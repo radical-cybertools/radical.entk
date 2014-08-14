45a103b80c2d9d5750410defb12529ae3a2fa3cf
Ole Weidner <ole.weidner@rutgers.edu>
2014-08-14 23:02:24 +0300

    updated API

---
+++ src/radical/ensemblemd/kernel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""This module defines and implements the Kernel class.
+"""
+
+__author__    = "Ole Weider <ole.weidner@rutgers.edu>"
+__copyright__ = "Copyright 2014, http://radical.rutgers.edu"
+__license__   = "MIT"
+
+from radical.ensemblemd.exceptions import TypeError
+
+
+# ------------------------------------------------------------------------------
+#
+class Kernel(object):
+    
+    #---------------------------------------------------------------------------
+    #
+    def __init__(self):
+        pass
