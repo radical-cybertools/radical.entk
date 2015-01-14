e9f48d8eb4e28e487de455f14c35a07b412ed93c
vivek-bala <b.vivek91@gmail.com>
2015-01-14 18:24:22 +0530

    add coam case

--- src/radical/ensemblemd/kernel_plugins/md/amber.py
+++ src/radical/ensemblemd/kernel_plugins/md/amber.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""The AMBER molecular dynamics package (http://ambermd.org/).
+"""A kernel that creates a new ASCII file with a given size and name.
 """
 
 __author__    = "Ole Weider <ole.weidner@rutgers.edu>"
@@ -16,50 +16,53 @@ from radical.ensemblemd.kernel_plugins.kernel_base import KernelBase
 # ------------------------------------------------------------------------------
 #
 _KERNEL_INFO = {
-    "name":            "md.amber",
-    "description":     "The AMBER molecular dynamics package (http://ambermd.org/).",
-    "arguments":       "*",  # "*" means arguments are not evaluated and just passed through to the kernel.
-    "machine_configs":
+
+    "name":         "md.amber",
+    "description":  "Creates a new file of given size and fills it with random ASCII characters.",
+    "arguments":   {"--mininfile=":
+                        {
+                            "mandatory": True,
+                            "description": "Input parameter filename"
+                        },
+                    "--mdinfile=":
+                        {
+                            "mandatory": True,
+                            "description": "Input parameter filename"
+                        },
+                    "--topfile=":
+                        {
+                            "mandatory": True,
+                            "description": "Input topology filename"
+                        },
+                    "--cycle=":
+                        {
+                            "mandatory": True,
+                            "description": "Cycle number"
+                        },
+                    },
+    "machine_configs": 
     {
-        "*":
-        {
-            "environment" : {},
-            "pre_exec"    : ["date"],
-            "post_exec"   : ["date"],
-            "executable"  : "pmemd.MPI",
-            "uses_mpi"    : True,
-            "test_cmd"    : "pmemd --version"
+        "*": {
+            "environment"   : {"FOO": "bar"},
+            "pre_exec"      : [],
+            "executable"    : ".",
+            "uses_mpi"      : True
         },
 
-        "stampede":
+        "stampede.tacc.utexas.edu":
         {
             "environment" : {},
-            "pre_exec"    : ["module load TACC", "module load amber"],
-            "post_exec"   : [],
-            "executable"  : "pmemd.MPI",
-            "uses_mpi"    : True,
-            "test_cmd"    : "pmemd --version"
+            "pre_exec" : ["module load TACC && module load amber"],
+            "executable" : ["/bin/bash"],
+            "uses_mpi"   : True
         },
 
-        "archer":
+        "archer.ac.uk":
         {
             "environment" : {},
-            "pre_exec"    : ["module load packages-archer","module load amber"],
-            "post_exec"   : [],
-            "executable"  : "pmemd.MPI",
-            "uses_mpi"    : True,
-            "test_cmd"    : "pmemd --version"
-        },
-
-        "biou":
-        {
-            "environment" : {},
-            "pre_exec"    : ["module load amber"],
-            "post_exec"   : [],
-            "executable"  : "pmemd.MPI",
-            "uses_mpi"    : True,
-            "test_cmd"    : "pmemd --version"
-
+            "pre_exec" : ["module load packages-archer","module load amber"],
+            "executable" : ["/bin/bash"],
+            "uses_mpi"   : True
         }
     }
 }
@@ -85,8 +88,10 @@ class Kernel(KernelBase):
     # --------------------------------------------------------------------------
     #
     def _bind_to_resource(self, resource_key):
-        """(PRIVATE) Implements parent class method.
+
+        """(PRIVATE) Implements parent class method. 
         """
+
         if resource_key not in _KERNEL_INFO["machine_configs"]:
             if "*" in _KERNEL_INFO["machine_configs"]:
                 # Fall-back to generic resource key
@@ -96,9 +101,18 @@ class Kernel(KernelBase):
 
         cfg = _KERNEL_INFO["machine_configs"][resource_key]
 
-        self._executable  = cfg["executable"]
-        self._arguments   = self.get_raw_args()
-        self._environment = None
-        self._uses_mpi    = None
-        self._pre_exec    = cfg["pre_exec"]
-        self._post_exec   = cfg["post_exec"]
+        executable = "/bin/bash"
+        #change to pmemd.MPI by splitting into two kernels
+        arguments = ['-l','-c','pmemd -O -i {0} -o min{2}.out -inf min{2}.inf -r md{2}.crd -p {1} -c min{2}.crd -ref min{2}.crd && pmemd -O -i {3} -o md{2}.out -inf md{2}.inf -x md{2}.ncdf -r md{2}.rst -p {1} -c md{2}.crd'.format(
+                                                                     self.get_arg("--mininfile="),
+                                                                     self.get_arg("--topfile="),
+                                                                     self.get_arg("--cycle="),
+                                                                     self.get_arg("--mdinfile="))]
+       
+        self._executable  = executable
+        self._arguments   = arguments
+        self._environment = cfg["environment"]
+        self._uses_mpi    = cfg["uses_mpi"]
+        self._pre_exec    = cfg["pre_exec"] 
+        self._post_exec   = None
+
