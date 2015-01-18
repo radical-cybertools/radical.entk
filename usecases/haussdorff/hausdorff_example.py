7c304e0ccbb18c2cd5d0ff5e969995a55f656d6f
iparask <ioannis.paraskevakos@rutgers.edu>
2015-01-18 01:28:46 -0500

    Hausdorff EnsembleMD Script 1st version. Useless comment removal from Exec Plugin

--- usecases/haussdorff/hausdorff_example.py
+++ usecases/haussdorff/hausdorff_example.py
@@ -1,60 +1,7 @@
 #!/usr/bin/env python
 
 """  
-This example shows how to use the EnsembleMD Toolkit ``AllPairsPattern``
-pattern to execute n!/2(n-2)! simulation permutations of a set of n elements. In
-the ``permuter`` step, the necessary files for the all pair are created. Each
-``compare`` runs a comparison between a unique coupling of elements.
-
-
-Run Locally 
-^^^^^^^^^^^
-
-.. warning:: In order to run this example, you need access to a MongoDB server and 
-             set the ``RADICAL_PILOT_DBURL`` in your environment accordingly. 
-             The format is ``mongodb://hostname:port``. Read more about it
-             MongoDB in chapter :ref:`envpreparation`.
-
-**Step 1:** View and download the example sources :ref:`below <example_source_all_pairs_pattern>`.
-
-**Step 2:** Run this example with ``RADICAL_ENMD_VERBOSE`` set to ``info`` if you want to 
-see log messages about simulation progress::
-
-    RADICAL_ENMD_VERBOSE=info python simulation_analysis_loop.py
-
-Once the script has finished running, you should see <Need to implement it 
-completely to know the output files. Probably one file for everything?>the in 
-the same directory you launched the script in.
-
-Run Remotely
-^^^^^^^^^^^^
-
-By default, simulation and analysis steps run on one core your local machine:: 
-
-    SingleClusterEnvironment(
-        resource="localhost", 
-        cores=1, 
-        walltime=30,
-        username=None,
-        allocation=None
-    )
-
-You can change the script to use a remote HPC cluster and increase the number 
-of cores to see how this affects the runtime of the script as the individual
-all pair instances can run in parallel::
-
-    SingleClusterEnvironment(
-        resource="stampede.tacc.utexas.edu", 
-        cores=16, 
-        walltime=30,
-        username=None,  # add your username here 
-        allocation=None # add your allocation or project id here if required
-    )
-
-.. _example_source_hausdorff_example:
-
-Example Source 
-^^^^^^^^^^^^^^
+Hausdorff Distance calculation script for EnsembleMD Toolkit
 """
 
 __author__       = "Ioannis Paraskevakos <i.parask@rutgers.edu>"
@@ -63,62 +10,208 @@ __license__      = "MIT"
 __example_name__ = "All Pairs (generic)"
 
 import math
-
+import os
 from radical.ensemblemd import Kernel
-from radical.ensemblemd import AllPairsPattern
+from radical.ensemblemd import AllPairs
 from radical.ensemblemd import EnsemblemdError
 from radical.ensemblemd import SingleClusterEnvironment
 
+from radical.ensemblemd.engine import get_engine
+from radical.ensemblemd.kernel_plugins.kernel_base import KernelBase
+
+#-------------------------------------------------------------------------------
+#
+_KERNEL_INFO_QPC = {
+    "name":         "my.qpc",
+    "description":  "This Kernel calculates the MQPC alignment.",
+    "arguments":   {"--inputfile=":     
+                        {
+                        "mandatory": True,
+                        "description": "The first input Trajectory file."
+                        },
+                    "--filename=":     
+                        {
+                        "mandatory": True,
+                        "description": "The output file containing the difference count."
+                        }
+       
+                    },
+    "machine_configs": 
+    {
+        "*": {
+            "environment"   : {"FOO": "bar"},
+            "pre_exec"      : [],
+            "executable"    : ":",
+            "uses_mpi"      : False
+        }
+    }
+}
+
+#-------------------------------------------------------------------------------
+#
+class MyQPC(KernelBase):
+
+    def __init__(self):
+        """Le constructor.
+        """
+        super(MyQPC, self).__init__(_KERNEL_INFO_QPC)
+
+    @staticmethod
+    def get_name():
+        return _KERNEL_INFO_QPC["name"]
+
+    def _bind_to_resource(self, resource_key):
+        """This function binds the Kernel to a specific resource defined in 
+           "resource_key".
+        """
+        if resource_key not in _KERNEL_INFO_QPC["machine_configs"]:
+            if "*" in _KERNEL_INFO_QPC["machine_configs"]:
+                # Fall-back to generic resource key
+                resource_key = "*"
+            else:
+                raise NoKernelConfigurationError(kernel_name=_KERNEL_INFO_QPC["name"], resource_key=resource_key)
+
+        cfg = _KERNEL_INFO_QPC["machine_configs"][resource_key]
+
+        executable = "/bin/bash"
+        arguments  = ['-l', '-c', '{0}'.format(cfg["executable"])]
+
+        self._executable  = [executable]
+        self._arguments   = arguments
+        self._environment = cfg["environment"]
+        self._uses_mpi    = cfg["uses_mpi"]
+        self._pre_exec    = cfg["pre_exec"] 
+        self._post_exec   = None
+
+#-------------------------------------------------------------------------------
+#
+_KERNEL_INFO_HS = {
+    "name":         "my.hausdorff",
+    "description":  "This Kernel calculates the Hausdorff distance.",
+    "arguments":   {"--dist_file=":
+                        {
+                        "mandatory": True,
+                        "description": "The file that contains the Hausdorff Calculator"
+                        },
+                    "--inputfile1=":     
+                        {
+                        "mandatory": True,
+                        "description": "The first input Trajectory file."
+                        },
+                    "--inputfile2=":     
+                        {
+                        "mandatory": True,
+                        "description": "The second input Trajectory file."
+                        },
+                    "--outputfile=":     
+                        {
+                        "mandatory": True,
+                        "description": "The output file containing the difference count."
+                        },
+       
+                    },
+    "machine_configs": 
+    {
+        "*": {
+            "environment"   : None,
+            "pre_exec"      : [],
+            "executable"    : ":",
+            "uses_mpi"      : False
+        },
+        "xsede.stampede": {
+            "environment"   : None,
+            "pre_exec"      : ["module load python/2.7.3-epd-7.3.2"],
+            "executable"    : ":",
+            "uses_mpi"      : False
+        }
+    }
+}
+
+#-------------------------------------------------------------------------------
+#
+class MyHausdorff(KernelBase):
+
+    def __init__(self):
+        """Le constructor.
+        """
+        super(MyHausdorff, self).__init__(_KERNEL_INFO_HS)
+
+    @staticmethod
+    def get_name():
+        return _KERNEL_INFO_HS["name"]
+
+    def _bind_to_resource(self, resource_key):
+        """This function binds the Kernel to a specific resource defined in 
+           "resource_key".
+        """
+        if resource_key not in _KERNEL_INFO_HS["machine_configs"]:
+            if "*" in _KERNEL_INFO_HS["machine_configs"]:
+                # Fall-back to generic resource key
+                resource_key = "*"
+            else:
+                raise NoKernelConfigurationError(kernel_name=_KERNEL_INFO_HS["name"], resource_key=resource_key)
+
+        cfg = _KERNEL_INFO_HS["machine_configs"][resource_key]
+        executable = "python" 
+        arguments  = [self.get_arg("--dist_file="),
+                      self.get_arg("--inputfile1="),
+                      self.get_arg("--inputfile2="),
+                      self.get_arg("--outputfile=")
+        ]
+
+        self._executable  = executable
+        self._arguments   = arguments
+        self._environment = cfg["environment"]
+        self._uses_mpi    = cfg["uses_mpi"]
+        self._pre_exec    = cfg["pre_exec"] 
+        self._post_exec   = None
+
+# ------------------------------------------------------------------------------
+# Register the user-defined kernel with EnsembleMD.
+get_engine().add_kernel_plugin(MyQPC)
+get_engine().add_kernel_plugin(MyHausdorff)
 
 # ------------------------------------------------------------------------------
 #
-class HausdorffAP(AllPairsPattern):
+class HausdorffAP(AllPairs):
     """HausdorffAP implements the all pairs pattern described above. It
-       inherits from radical.ensemblemd.AllPairPattern, the abstract 
+       inherits from radical.ensemblemd.AllPair, the abstract 
        base class for all All Pairs applications.
     """
     def __init__(self,setelements):
-        AllPairsPattern.__init__(self, setelements)
+        AllPairs.__init__(self, setelements)
 
-    def init_step(self,element):
+    def element_initialization(self,element):
         """The initialization step creates the necessary files that will be 
             needed for the comparison over the elements of the set.
         """
 
-        # From what I understand the misc.<> is the script that will be run from
-        # the kernel. If this is correct, I should create one for Beickstein's
-        # use case based on the information from Sean's mail.
-        # It would make a nice example and it can be called misc.select.
-        k = Kernel(name="misc.select")
-        k.arguments("--inputfile==atom_traj{0}.dcd".format(element))
+        print "Element creation {0}".format(element)
+        k = Kernel(name="my.qpc")
+        k.arguments = ["--inputfile=traj_flat{0}.npz.npy".format(element),
+                        "--filename=traj_flat{0}.npz.npy".format(element)]
+        # When the input data are in a web server use download input data as the example below.
+        k.download_input_data = ["http://eceweb1.rutgers.edu/~ip176/traj_flat{0}.npz.npy > traj_flat{0}.npz.npy".format(element)]
+        print "Created {0}".format(element)
         return k
 
-    def comparison(self, element1, element2):
+    def element_comparison(self, element1, element2):
         """In the comparison, we take the previously generated modified trajectory
-           and perform a Hausdorff distance calculation between the elements of
-           the 
-
-           ..note:: The placeholder ``$INIT_STEP`` used in ``link_input_data`` is 
-                    a reference to the working directory of init_step.
-
+           and perform a Hausdorff distance calculation between all the unique pairs
+           of trajectories
         """
-        input_filename1 = "traj_flat-{0}.npz.npy".format(element1)
-        input_filename2 = "traj_flat-{0}.npz.npy".format(element2)
+        input_filename1 = "traj_flat{0}.npz.npy".format(element1)
+        input_filename2 = "traj_flat{0}.npz.npy".format(element2)
         output_filename = "comparison-{0}-{1}.dat".format(element1, element2)
     
-        # From what I understand the misc.<> is the script that will be run from
-        # the kernel. If this is correct, I should create one based on Beickstein's
-        # script for his use case. It would make a nice example and it can be
-        # called misc.hausdorff
-
-        k = Kernel(name="misc.hausdorff")
-        k.link_input_data      = ["$INIT_STEP/{0}, $INIT_STEP/{1}".format(input_filename1,input_filename2)]
-        k.arguments            = ["--inputfile1={0}".format(input_filename1), 
+        k = Kernel(name="my.hausdorff")
+        k.arguments            = ["--dist_file=hausdorff_kernel.py",
+                                  "--inputfile1={0}".format(input_filename1), 
                                   "--inputfile2={0}".format(input_filename2), 
                                   "--outputfile={0}".format(output_filename)]
-    
-        # I think it download the output files from the cluster to the machine 
-        # that run this script. Need to learn more here.
+        k.download_input_data = ["http://eceweb1.rutgers.edu/~ip176/hausdorff_kernel.py > hausdorff_kernel.py"]
+        
+        # The result files comparison-x-y.dat are downloaded.
         k.download_output_data = output_filename
         return k
 
@@ -130,24 +223,22 @@ if __name__ == "__main__":
         # Create a new static execution context with one resource and a fixed
         # number of cores and runtime.
         cluster = SingleClusterEnvironment(
-            resource="localhost", 
+            resource="local.localhost", 
             cores=1, 
             walltime=30,
-            username=None,
+            username=None,    
             allocation=None
         )
         
-        # For example the set has 5 elements.
-        # May try it with Beickstein's use case.
-        ElementsSet = range(1,6)
-        hausdorff = HausdorffAP(ElementsSet)
+        # For example the set has 10 elements.
+        ElementsSet = range(1,11)
+        hausdorff = HausdorffAP(setelements=ElementsSet)
 
         cluster.run(hausdorff)
 
-        # Do not know yet what should be printed... 
-        # Need to see what the use case may need.
-        for it in range(1, haudorff._size+1):
-            print "..."
+        #Message printed when everything is done
+        print "Everything is downloaded!"
+        
 
     except EnsemblemdError, er:
 
