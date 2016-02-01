65bc062aa46308c5a4a36b2dc945f66212452278
vivek-bala <b.vivek91@gmail.com>
2016-02-01 15:30:51 -0500

    add separate bot pattern

--- src/radical/ensemblemd/__init__.py
+++ src/radical/ensemblemd/__init__.py
@@ -18,6 +18,7 @@ from radical.ensemblemd.kernel import Kernel
 # Execution Patterns
 from radical.ensemblemd.patterns.all_pairs_pattern import AllPairs
 from radical.ensemblemd.patterns.pipeline import Pipeline
+from radical.ensemblemd.patterns.bag_of_tasks import BagofTasks
 from radical.ensemblemd.patterns.replica_exchange import ReplicaExchange
 from radical.ensemblemd.patterns.simulation_analysis_loop import SimulationAnalysisLoop
 
