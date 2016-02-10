c04a4cfc67191f7aaa5bea98499e7cbab9eae828
vivek-bala <b.vivek91@gmail.com>
2016-02-10 11:31:01 -0500

    checkpoint

---
+++ examples/synapse/synapse_kernel.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+"""A kernel that creates a new ASCII file with a given size and name.
+"""
+
+__author__    = "The ExTASY project <ardita.shkurti@nottingham.ac.uk>"
+__copyright__ = "Copyright 2015, http://www.extasy-project.org/"
+__license__   = "MIT"
+
+from copy import deepcopy
+
+from radical.ensemblemd.exceptions import ArgumentError
+from radical.ensemblemd.exceptions import NoKernelConfigurationError
+from radical.ensemblemd.engine import get_engine
+from radical.ensemblemd.kernel_plugins.kernel_base import KernelBase
+
+# ------------------------------------------------------------------------------
+#
+_KERNEL_INFO = {
+    "name":         "synapse",
+    "description":  "kernel to use radical.synapse",
+    "arguments":    {
+                    },
+    "machine_configs":
+    {
+        "*": {
+            "environment"   : {"FOO": "bar"},
+            "pre_exec"      : [],
+            "executable"    : ".",
+            "uses_mpi"      : False
+        }               
+    }
+}
+
+
+# ------------------------------------------------------------------------------
+#
+class synapse_Kernel(KernelBase):
+
+    # --------------------------------------------------------------------------
+    #
+    def __init__(self):
+        """Le constructor.
+        """
+        super(grompp_Kernel, self).__init__(_KERNEL_INFO)
+
+    # --------------------------------------------------------------------------
+    #
+    @staticmethod
+    def get_name():
+        return _KERNEL_INFO["name"]
+
+    # --------------------------------------------------------------------------
+    #
+    def _bind_to_resource(self, resource_key):
+        """(PRIVATE) Implements parent class method.
+        """
+        if resource_key not in _KERNEL_INFO["machine_configs"]:
+            if "*" in _KERNEL_INFO["machine_configs"]:
+                # Fall-back to generic resource key
+                resource_key = "*"
+            else:
+                raise NoKernelConfigurationError(kernel_name=_KERNEL_INFO["name"], resource_key=resource_key)
+
+        cfg = _KERNEL_INFO["machine_configs"][resource_key]
+        
+        if self.get_arg("--ref=") is None:
+            arguments = ['-f','{0}'.format(self.get_arg("--mdp=")),'-c','{0}'.format(self.get_arg("--gro=")),'-p','{0}'.format(self.get_arg("--top=")),'-o','{0}'.format(self.get_arg("--tpr="))]
+        else:			   
+            arguments = ['-f','{0}'.format(self.get_arg("--mdp=")),'-r','{0}'.format(self.get_arg("--ref=")),'-c','{0}'.format(self.get_arg("--gro=")),'-p','{0}'.format(self.get_arg("--top=")),'-o','{0}'.format(self.get_arg("--tpr="))]
+        
+        self._executable  = cfg['executable']
+        self._arguments   = arguments
+        self._environment = cfg["environment"]
+        self._uses_mpi    = cfg["uses_mpi"]
+        self._pre_exec    = cfg["pre_exec"]
+        self._post_exec   = None
