79199147a47b0951246da6b4145f72158f300522
vivek-bala <vivek.balasubramanian@rutgers.edu>
2017-04-28 11:33:13 -0400

    sync up to remote

--- design_helpers/procs_throughput/runme_procs.py
+++ design_helpers/procs_throughput/runme_procs.py
@@ -29,10 +29,28 @@ def push_function(q, name):
         q_len = []
         q_sizes = []
         proc_mem = []
+        t = Task()
+        t.arguments = ["--template=PLCpep7_template.mdp",
+                        "--newname=PLCpep7_run.mdp",
+                        "--wldelta=100",
+                        "--equilibrated=False",
+                        "--lambda_state=0",
+                        "--seed=1"]
+
+        t.cores = 20
+        t.copy_input_data = ['$STAGE_2_TASK_1/PLCpep7.tpr']
+        t.download_output_data = ['PLCpep7.xtc > PLCpep7_run1_gen0.xtc',
+                                    'PLCpep7.log > PLCpep7_run1_gen0.log',
+                                    'PLCpep7_dhdl.xvg > PLCpep7_run1_gen0_dhdl.xvg',
+                                    'PLCpep7_pullf.xvg > PLCpep7_run1_gen0_pullf.xvg',
+                                    'PLCpep7_pullx.xvg > PLCpep7_run1_gen0_pullx.xvg',
+                                    'PLCpep7.gro > PLCpep7_run1_gen0.gro'
+                                ]
+        print 'Size of task: ', asizeof.asizeof(t)
 
         while (tasks_pushed < 1000000)and(not kill_pusher.is_set()):
 
-            t = Task()
+            
             #t = DATA
             q.put(t)
 
