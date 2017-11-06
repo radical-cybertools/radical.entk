45973060c2b8b2eb85648ae430122e277e65df3e
vivek-bala <b.vivek91@gmail.com>
2017-11-06 12:06:33 -0500

    removing commented line

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ setup_args = {
 
     'package_data'      :  {'': ['*.sh', '*.json', 'VERSION', 'SDIST']},
 
-    'install_requires'  :  ['radical.pilot', 'pika'],
+    'install_requires'  :  ['radical.pilot', 'pika', 'pandas', 'numpy', 'matplotlib'],
 
     'zip_safe'          : False,
     
