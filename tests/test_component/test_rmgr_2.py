a3b40a67bd7ff08958669b6f1ffc1b90bfc9ea9b
Andre Merzky <andre@merzky.net>
2019-06-27 10:44:21 +0200

    cleanup of a test
    the test fails in travis, works locally.  Bah.

--- tests/test_component/test_rmgr_2.py
+++ tests/test_component/test_rmgr_2.py
@@ -1,15 +1,13 @@
-from radical.entk.execman.base import Base_ResourceManager as BaseRmgr
-from radical.entk.execman.rp import ResourceManager as RPRmgr
-from radical.entk.execman.mock import ResourceManager as MockRmgr
-from radical.entk.exceptions import *
-from hypothesis import given
-import hypothesis.strategies as st
-import os
-import radical.utils as ru
+#!/usr/bin/env python
 
-# MLAB = 'mongodb://entk:entk123@ds143511.mlab.com:43511/entk_0_7_4_release'
-MLAB = os.environ.get('RADICAL_PILOT_DBURL')
+import radical.utils         as ru
+import radical.pilot         as rp
 
+from radical.entk.execman.rp   import ResourceManager as RPRmgr
+
+
+# ------------------------------------------------------------------------------
+#
 def test_rmgr_rp_get_resource_allocation_state():
 
     res_dict = {
@@ -19,11 +17,9 @@ def test_rmgr_rp_get_resource_allocation_state():
                     'project': ''
     }
 
-    os.environ['RADICAL_PILOT_DBURL'] = MLAB
-
-    config = {"sandbox_cleanup": False, "db_cleanup": False}
+    config  = {'sandbox_cleanup': False, 'db_cleanup': False}
     rmgr_id = ru.generate_id('test.%(item_counter)04d', ru.ID_CUSTOM)
-    rmgr = RPRmgr(res_dict, sid=rmgr_id, rts_config=config)
+    rmgr    = RPRmgr(res_dict, sid=rmgr_id, rts_config=config)
 
     assert not rmgr.get_resource_allocation_state()
 
@@ -31,42 +27,49 @@ def test_rmgr_rp_get_resource_allocation_state():
     rmgr._populate()
     rmgr._submit_resource_request()
 
-    import radical.pilot as rp
     assert rmgr.get_resource_allocation_state() in [rp.PMGR_ACTIVE, rp.FAILED]
     rmgr._terminate_resource_request()
 
 
+# ------------------------------------------------------------------------------
+#
 def test_rmgr_rp_resource_request():
-    """
-    ***Purpose***: Test the submission and cancelation of a resource request. Check states that pilot starts and
-    ends with.
-    """
+    '''
+    ***Purpose***: Test the submission and cancelation of a resource request.
+                   Check states that pilot starts and ends with.
+    '''
 
-    res_dict = {
-        'resource': 'local.localhost',
-                    'walltime': 10,
-                    'cpus': 1,
-                    'project': ''
-    }
+    res_dict = {'resource': 'local.localhost',
+                'walltime': 10,
+                'cpus'    : 1,
+                'project' : ''
+               }
 
-    os.environ['RADICAL_PILOT_DBURL'] = MLAB
-    os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
+  # os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
 
-    config = {"sandbox_cleanup": False, "db_cleanup": False}
+    config  = {'sandbox_cleanup': False, 'db_cleanup': False}
     rmgr_id = ru.generate_id('test.%(item_counter)04d', ru.ID_CUSTOM)
-    rmgr = RPRmgr(res_dict, sid=rmgr_id, rts_config=config)
+    rmgr    = RPRmgr(res_dict, sid=rmgr_id, rts_config=config)
+
     rmgr._validate_resource_desc()
     rmgr._populate()
-
     rmgr._submit_resource_request()
-
-    import radical.pilot as rp
-
     rmgr._terminate_resource_request()
 
     # State transition seems to be taking some time. So sleep
     from time import sleep
-    sleep(30)
+    sleep(10)
 
-    assert rmgr.get_resource_allocation_state(
-    ) in [rp.CANCELED, rp.FAILED, rp.DONE]
+    assert rmgr.get_resource_allocation_state() in rp.FINAL
+
+
+# ------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+    test_rmgr_rp_get_resource_allocation_state()
+    test_rmgr_rp_resource_request()
+
+
+# ------------------------------------------------------------------------------
+
