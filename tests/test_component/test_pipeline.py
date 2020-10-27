a291ee7acccb18fa7fa395f3bbe8a77e52a64eb8
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2020-10-27 16:55:50 -0400

    Pipelines and stages unit test complete based on guidelines

--- tests/test_component/test_pipeline.py
+++ tests/test_component/test_pipeline.py
@@ -1,8 +1,8 @@
 # pylint: disable=protected-access, unused-argument
 # pylint: disable=no-value-for-parameter
 
-import pytest
 from unittest import TestCase
+from random import shuffle
 
 from   hypothesis import given, settings
 import hypothesis.strategies as st
@@ -34,20 +34,20 @@ class TestBase(TestCase):
 
         p = Pipeline()
 
-        assert p.uid == 'pipeline.0000'
-        assert p.name is None
-        assert p.stages == list()
-        assert p.state == states.INITIAL
-        assert p.state_history == [states.INITIAL]
-        assert p._stage_count == 0
-        assert p.current_stage == 0
-        assert p._lock == 'test_lock'
-        assert p._completed_flag == 'test_event'
+        self.assertEqual(p.uid, 'pipeline.0000')
+        self.assertEqual(p.name, None)
+        self.assertEqual(p.stages, list())
+        self.assertEqual(p.state, states.INITIAL)
+        self.assertEqual(p.state_history, [states.INITIAL])
+        self.assertEqual(p._stage_count, 0)
+        self.assertEqual(p.current_stage, 0)
+        self.assertEqual(p._lock, 'test_lock')
+        self.assertEqual(p._completed_flag, 'test_event')
 
         p._completed_flag = mock.Mock()
         p._completed_flag.is_set = mock.MagicMock(return_value=False)
 
-        assert p.completed is False
+        self.assertFalse(p.completed)
 
 
     # ------------------------------------------------------------------------------
@@ -71,13 +71,13 @@ class TestBase(TestCase):
         for data in data_type:
 
             if not isinstance(data, str):
-                with pytest.raises(TypeError):
+                with self.assertRaises(TypeError):
                     p.name = data
 
-            with pytest.raises(TypeError):
+            with self.assertRaises(TypeError):
                 p.stages = data
 
-            with pytest.raises(TypeError):
+            with self.assertRaises(TypeError):
                 p.add_stages(data)
 
 
@@ -93,10 +93,10 @@ class TestBase(TestCase):
         s = mock.MagicMock(spec=Stage)
         p.stages = s
 
-        assert type(p.stages) == list
-        assert p._stage_count == 1
-        assert p._cur_stage == 1
-        assert p.stages[0] == s
+        self.assertEqual(type(p.stages), list)
+        self.assertEqual(p._stage_count, 1)
+        self.assertEqual(p._cur_stage, 1)
+        self.assertEqual(p.stages[0], s)
 
 
     # ------------------------------------------------------------------------------
@@ -116,16 +116,25 @@ class TestBase(TestCase):
         data_type = [l, i, b]
 
         for data in data_type:
-            with pytest.raises(TypeError):
+            with self.assertRaises(TypeError):
                 p.state = data
 
         if isinstance(t,str):
-            with pytest.raises(ValueError):
+            with self.assertRaises(ValueError):
                 p.state = t
 
-        for val in list(states._pipeline_state_values.keys()):
+        state_history = list()
+        p = Pipeline()
+        p._state = None
+        p._state_history = list()
+        states_list = list(states._pipeline_state_values.keys())
+        shuffle(states_list)
+        for val in states_list:
             p.state = val
-
+            if val != states.SUSPENDED:
+                state_history.append(val)
+            self.assertEqual(p._state, val)
+            self.assertEqual(p._state_history, state_history)
 
     # ------------------------------------------------------------------------------
     #
@@ -135,17 +144,16 @@ class TestBase(TestCase):
     def test_pipeline_stage_addition(self, mocked_generate_id, mocked_Lock,
                                      mocked_Event):
 
-        # FIXME: instantiates and test also task addition for stages.
         p = Pipeline()
         s1 = mock.MagicMock(spec=Stage)
         s2 = mock.MagicMock(spec=Stage)
         p.add_stages([s1, s2])
 
-        assert type(p.stages) == list
-        assert p._stage_count == 2
-        assert p._cur_stage == 1
-        assert p.stages[0] == s1
-        assert p.stages[1] == s2
+        self.assertEqual(type(p.stages), list)
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 1)
+        self.assertEqual(p.stages[0], s1)
+        self.assertEqual(p.stages[1], s2)
 
 
     # ------------------------------------------------------------------------------
@@ -165,11 +173,11 @@ class TestBase(TestCase):
         p._completed_flag.is_set = mock.MagicMock(return_value=False)
 
         d = p.to_dict()
-        assert d == {'uid': 'pipeline.0000',
-                    'name': 'test_pipeline',
-                    'state': states.INITIAL,
-                    'state_history': [states.INITIAL],
-                    'completed': False}
+        self.assertEqual(d, {'uid': 'pipeline.0000',
+                             'name': 'test_pipeline',
+                             'state': states.INITIAL,
+                             'state_history': [states.INITIAL],
+                             'completed': False})
 
 
     # ------------------------------------------------------------------------------
@@ -188,11 +196,11 @@ class TestBase(TestCase):
         p._completed_flag.is_set = mock.MagicMock(return_value=d['completed'])
         p.from_dict(d)
 
-        assert p.uid == d['uid']
-        assert p.name == d['name']
-        assert p.state == d['state']
-        assert p.state_history == d['state_history']
-        assert p.completed == d['completed']
+        self.assertEqual(p.uid, d['uid'])
+        self.assertEqual(p.name, d['name'])
+        self.assertEqual(p.state, d['state'])
+        self.assertEqual(p.state_history, d['state_history'])
+        self.assertEqual(p.completed, d['completed'])
 
 
     # ------------------------------------------------------------------------------
@@ -207,19 +215,19 @@ class TestBase(TestCase):
         p._stage_count = 2
         p._increment_stage()
 
-        assert p._stage_count == 2
-        assert p._cur_stage == 1
-        assert p._completed_flag.is_set() is False
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 1)
+        self.assertFalse(p._completed_flag.is_set())
 
         p._increment_stage()
-        assert p._stage_count == 2
-        assert p._cur_stage == 2
-        assert p._completed_flag.is_set() is False
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 2)
+        self.assertFalse(p._completed_flag.is_set())
 
         p._increment_stage()
-        assert p._stage_count == 2
-        assert p._cur_stage == 2
-        assert p._completed_flag.is_set() is True
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 2)
+        self.assertTrue(p._completed_flag.is_set())
 
 
     # ------------------------------------------------------------------------------
@@ -234,14 +242,14 @@ class TestBase(TestCase):
         p._stage_count = 2
 
         p._decrement_stage()
-        assert p._stage_count == 2
-        assert p._cur_stage == 1
-        assert p._completed_flag.is_set() is False
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 1)
+        self.assertFalse(p._completed_flag.is_set(), False)
 
         p._decrement_stage()
-        assert p._stage_count == 2
-        assert p._cur_stage == 0
-        assert p._completed_flag.is_set() is False
+        self.assertEqual(p._stage_count, 2)
+        self.assertEqual(p._cur_stage, 0)
+        self.assertFalse(p._completed_flag.is_set(), False)
 
 
     # ------------------------------------------------------------------------------
@@ -259,15 +267,15 @@ class TestBase(TestCase):
         data_type = [t, l, i, b, se]
 
         for data in data_type:
-            with pytest.raises(TypeError):
+            with self.assertRaises(TypeError):
                 p._validate_entities(data)
 
         s = mock.MagicMock(spec=Stage)
-        assert isinstance(p._validate_entities(s), list)
+        self.assertIsInstance(p._validate_entities(s), list)
 
         s1 = mock.MagicMock(spec=Stage)
         s2 = mock.MagicMock(spec=Stage)
-        assert [s1,s2] == p._validate_entities([s1,s2])
+        self.assertEqual([s1,s2], p._validate_entities([s1,s2]))
 
 
     # ------------------------------------------------------------------------------
@@ -278,14 +286,14 @@ class TestBase(TestCase):
         p = Pipeline()
         p._uid = 'pipeline.0000'
         p._state = 'test'
-        with pytest.raises(ValueError):
+        with self.assertRaises(ValueError):
             p._validate()
 
         p = Pipeline()
         p._uid = 'pipeline.0000'
         p._stages = list()
         p._state = states.INITIAL
-        with pytest.raises(MissingError):
+        with self.assertRaises(MissingError):
             p._validate()
 
         p = Pipeline()
@@ -303,39 +311,39 @@ class TestBase(TestCase):
 
         p = Pipeline()
         p._name = 'test_pipe'
-        assert p.name == 'test_pipe'
-        assert p.luid == 'test_pipe'
+        self.assertEqual(p.name, 'test_pipe')
+        self.assertEqual(p.luid, 'test_pipe')
 
         p = Pipeline()
         p._stages = ['test_stage']
-        assert p.stages == ['test_stage']
+        self.assertEqual(p.stages, ['test_stage'])
 
         p = Pipeline()
         p._state = 'test_state'
-        assert p.state == 'test_state'
+        self.assertEqual(p.state, 'test_state')
 
         p = Pipeline()
         p._uid = 'pipeline.0000'
         p._name = None
-        assert p.uid == 'pipeline.0000'
-        assert p.luid == 'pipeline.0000'
+        self.assertEqual(p.uid, 'pipeline.0000')
+        self.assertEqual(p.luid, 'pipeline.0000')
 
         p = Pipeline()
         p._lock = 'test_lock'
-        assert p.lock == 'test_lock'
+        self.assertEqual(p.lock, 'test_lock')
 
         p = Pipeline()
         p._completed_flag = mock.Mock()
         p._completed_flag.is_set = mock.MagicMock(return_value='flag_set')
-        assert p.completed == 'flag_set'
+        self.assertEqual(p.completed, 'flag_set')
 
         p = Pipeline()
         p._cur_stage = 'some_stage'
-        assert p.current_stage == 'some_stage'
+        self.assertEqual(p.current_stage, 'some_stage')
 
         p = Pipeline()
         p._state_history = ['state1','state2']
-        assert p.state_history == ['state1','state2']
+        self.assertEqual(p.state_history, ['state1','state2'])
 
     # ------------------------------------------------------------------------------
     #
@@ -346,13 +354,13 @@ class TestBase(TestCase):
         p._state = states.SCHEDULED
         p._state_history = [states.SCHEDULED]
         p.suspend()
-        assert p._state == states.SUSPENDED
-        assert p._state_history == [states.SCHEDULED, states.SUSPENDED]
+        self.assertEqual(p._state, states.SUSPENDED)
+        self.assertEqual(p._state_history, [states.SCHEDULED, states.SUSPENDED])
 
         p = Pipeline()
         p._uid = 'pipeline.0000'
         p._state = states.SUSPENDED
-        with pytest.raises(EnTKError):
+        with self.assertRaises(EnTKError):
             p.suspend()
 
 # ------------------------------------------------------------------------------
@@ -364,13 +372,13 @@ class TestBase(TestCase):
         p._state = states.SUSPENDED
         p._state_history = [states.SCHEDULED, states.SUSPENDED]
         p.resume()
-        assert p._state == states.SCHEDULED
-        assert p._state_history == [states.SCHEDULED, states.SUSPENDED,
-                                    states.SCHEDULED]
+        self.assertEqual(p._state, states.SCHEDULED)
+        self.assertEqual(p._state_history, [states.SCHEDULED, states.SUSPENDED,
+                                    states.SCHEDULED])
 
         p = Pipeline()
         p._uid = 'pipeline.0000'
         p._state = states.SCHEDULED
         p._state_history = [states.SCHEDULED]
-        with pytest.raises(EnTKError):
+        with self.assertRaises(EnTKError):
             p.resume()
