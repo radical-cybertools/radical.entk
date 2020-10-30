fa509c8fbadf0969ef3c8448f7fb94b756bde1c1
Andre Merzky <andre@merzky.net>
2020-10-30 16:40:20 +0100

    formatting

--- tests/test_component/test_modules.py
+++ tests/test_component/test_modules.py
@@ -1,20 +1,29 @@
+
 from unittest import TestCase
 
 # pylint: disable=protected-access, unused-argument, unused-import
 # pylint: disable=no-value-for-parameter
 
 
+# ------------------------------------------------------------------------------
+#
 class TestBase(TestCase):
+
+    # --------------------------------------------------------------------------
+    #
     def test_user_module_loads(self):
 
         """
         **Purpose**: Test if all the user components can be imported by the user
         """
 
-        import radical.entk  # noqa: F401
-        from radical.entk import Task  # noqa: F401
-        from radical.entk import Stage  # noqa: F401
-        from radical.entk import Pipeline  # noqa: F401
-        from radical.entk import AppManager  # noqa: F401
-        from radical.entk import states  # noqa: F401
+        import radical.entk                                         # noqa: F401
+        from radical.entk import Task                               # noqa: F401
+        from radical.entk import Stage                              # noqa: F401
+        from radical.entk import Pipeline                           # noqa: F401
+        from radical.entk import AppManager                         # noqa: F401
+        from radical.entk import states                             # noqa: F401
+
+
+# ------------------------------------------------------------------------------
 
