54a36c3b21fcb71c3adea983c5e9fc1fc8f851b8
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-01-04 12:32:07 -0500

    Reverting wrong change in a test.

--- tests/test_component/test_tmgr_base.py
+++ tests/test_component/test_tmgr_base.py
@@ -175,8 +175,11 @@ class TestBase(TestCase):
         tmgr = Tmgr('test_tmgr', ['pending_queues'], ['completed_queues'], 
                      rmgr, rmq_params, 'test_rts')
 
+        def _tmgr_side_effect(amount):
+            time.sleep(amount)
+
         tmgr._tmgr_process = mt.Thread(target=_tmgr_side_effect,
-                                       name='test_tmgr', args=(1))
+                                       name='test_tmgr', args=(1,))
         tmgr._tmgr_process.start()
 
         self.assertTrue(tmgr.check_manager())
