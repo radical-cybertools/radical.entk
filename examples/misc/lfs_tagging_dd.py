fe610189e15013f569bbd84086af0c654fa26a65
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-01-11 15:06:18 -0500

    Updating lfs tagging example

--- examples/misc/lfs_tagging_dd.py
+++ examples/misc/lfs_tagging_dd.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 from radical.entk import Pipeline, Stage, Task, AppManager
@@ -24,86 +25,79 @@ def get_pipeline(n=2):
 
     # We create a pipeline which has 3 stages. The tasks from the second and
     # and third stage will execute at the same node as the respective tasks from
-    # the first stage. 
-
-    p = Pipeline()
-    p.name = 'p'
-
-    s1 = Stage()
-    s1.name = 's1'
+    # the first stage.
+    pipelines = list()
     for x in range(n):
+        p = Pipeline()
+        p.name = 'p.%04d' % x
+
+        s1 = Stage()
+        s1.name = 's1'
         # The tasks from the first stage will execute at the first available node
         # they fit.
-        t = Task()
-        t.name = 't1.%04d' % x
-        t.executable = 'hostname'
-        t.cpu_reqs = {'cpu_processes': 1,
-                      'cpu_threads': ,  # Set enough threads for this task to get a whole node
-                      'cpu_process_type': None, 
+        t1 = Task()
+        t1.name = 't1.%04d' % x
+        t1.executable = 'hostname'
+        t1.cpu_reqs = {'cpu_processes': 1,
+                      'cpu_threads': 1,  # Set enough threads for this task to get a whole node
+                      'cpu_process_type': None,
                       'cpu_thread_type': None}
-        t.lfs_per_process = 1024
+        t1.lfs_per_process = 10
+        s1.add_tasks(t1)
 
-        s1.add_tasks(t)
+        p.add_stages(s1)
 
-    p.add_stages(s1)
-
-    s2 = Stage()
-    s2.name = 's2'
-    for x in range(2 * n):
+        s2 = Stage()
+        s2.name = 's2'
         # Tasks from this stage will execute on the node the task from stage 1
         # it depends executed.
-        t = Task()
-        t.name = 't2.%04d' % x
-        t.executable = 'hostname'
-        t.cpu_reqs = {'cpu_processes': 1, 
+        t2 = Task()
+        t2.name = 't2.%04d' % x
+        t2.executable = 'hostname'
+        t2.cpu_reqs = {'cpu_processes': 1,
                       'cpu_threads': 1,
                       'cpu_process_type': None,
                       'cpu_thread_type': None}
-        t.lfs_per_process = 1024
-        t.tag = 't1.%04d' % (x % 4)  # As a tag we use the name of the task this task depends upon.
+        t2.tag = t1.uid  # As a tag we use the ID of the first task this task depends upon.
+        t2.lfs_per_process = 10
+        s2.add_tasks(t2)
 
-        s2.add_tasks(t)
 
+        p.add_stages(s2)
 
-    p.add_stages(s2)
-
-    s3 = Stage()
-    s3.name = 's3'
-    for x in range(n):
+        s3 = Stage()
+        s3.name = 's3'
         # Tasks from this stage will execute on the node the task from stage 1
         # it depends executed.
-        t = Task()
-        t.name = 't3.%04d' % x
-        t.executable = 'hostname'
-        t.cpu_reqs = {'cpu_processes': 1, 
+        t3 = Task()
+        t3.name = 't3.%04d' % x
+        t3.executable = 'hostname'
+        t3.cpu_reqs = {'cpu_processes': 1,
                       'cpu_threads': 1,
                       'cpu_process_type': None,
                       'cpu_thread_type': None}
-        t.lfs_per_process = 1024
-        t.tag = 't1.%04d' % x   # As a tag we use the name of the task this task depends upon.
-        s3.add_tasks(t)
+        t3.lfs_per_process = 10
+        t3.tag = t1.uid   # As a tag we use the ID of the first task this task depends upon.
+        s3.add_tasks(t3)
 
+        p.add_stages(s3)
+        pipelines.append(p)
 
-    p.add_stages(s3)
-
-    return p
-
+    return pipelines
 
 
 if __name__ == '__main__':
 
-    # Request at least two nodes 
+    # Request at least two nodes
     res_dict = {
-                'resource'      : '',
-                'walltime'      : ,
-                'cpus'          : ,
-                'project'       : '',
-                'queue'         : ''
+                'resource'      : 'local.localhost',
+                'walltime'      : 20,
+                'cpus'          : 2,
             }
 
     appman = AppManager(hostname=hostname, port=port, username=username, password=password)
     appman.resource_desc = res_dict
 
     p = get_pipeline(n=2)  # Select n to be greater or equal to the number of nodes.
-    appman.workflow = set([p])
+    appman.workflow = set(p)
     appman.run()
