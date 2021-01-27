cef9fe27f7b4f50e554e415603220318b9c3cff9
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-01-27 16:53:08 -0500

    hopefully I trigger the exception

--- tests/test_integration/test_tmgr_base/test_sync_with_master.py
+++ tests/test_integration/test_tmgr_base/test_sync_with_master.py
@@ -6,7 +6,7 @@ import json
 import time
 
 from unittest import TestCase, mock
-import threading as mt
+import multiprocessing as mp
 
 from radical.entk              import Task, Stage
 from radical.entk.execman.base import Base_TaskManager     as BaseTmgr
@@ -22,44 +22,44 @@ class TestTask(TestCase):
     @mock.patch('radical.utils.Logger')
     @mock.patch('radical.utils.Profiler')
     def test_sync_with_master(self, mocked_init, mocked_Logger, mocked_Profiler):
-
+        self.maxDiff = None
         # --------------------------------------------------------------------------
         #
-        def component_execution(inputs, method, channel, conn_params, queue):
+        def component_execution(packets, conn_params, queue):
 
-            for obj_type, obj, in inputs:
-                method(obj, obj_type, channel, conn_params, queue)
-                if channel.is_open:
-                    channel.close()
-            return True
-
+            tmgr = BaseTmgr(None, None, None, None, None, None)
+            tmgr._log = mocked_Logger
+            tmgr._prof = mocked_Profiler
+            mq_connection = pika.BlockingConnection(rmq_conn_params)
+            mq_channel = mq_connection.channel()
+            for obj_type, obj, in packets:
+                tmgr._sync_with_master(obj, obj_type, mq_channel, conn_params,
+                                       queue)
+                if mq_channel.is_open:
+                    mq_channel.close()
 
         task = Task()
         task.parent_stage = {'uid':'stage.0000', 'name': 'stage.0000'}
-        hostname = os.environ.get('RMQ_HOSTNAME', 'localhost')
-        port = int(os.environ.get('RMQ_PORT', '5672'))
-        username = os.environ.get('RMQ_USERNAME','guest')
-        password = os.environ.get('RMQ_PASSWORD','guest')
         packets = [('Task', task)]
         stage = Stage()
         stage.parent_pipeline = {'uid':'pipe.0000', 'name': 'pipe.0000'}
         packets.append(('Stage', stage))
+        hostname = os.environ.get('RMQ_HOSTNAME', 'localhost')
+        port = int(os.environ.get('RMQ_PORT', '5672'))
+        username = os.environ.get('RMQ_USERNAME','guest')
+        password = os.environ.get('RMQ_PASSWORD','guest')
         credentials = pika.PlainCredentials(username, password)
         rmq_conn_params = pika.ConnectionParameters(host=hostname, port=port,
                 credentials=credentials)
         mq_connection = pika.BlockingConnection(rmq_conn_params)
         mq_channel = mq_connection.channel()
         mq_channel.queue_declare(queue='master')
-        tmgr = BaseTmgr(None, None, None, None, None, None)
-        tmgr._log = mocked_Logger
-        tmgr._prof = mocked_Profiler
-
-        master_thread = mt.Thread(target=component_execution,
+        master_thread = mp.Process(target=component_execution,
                                   name='tmgr_sync', 
-                                  args=(packets, tmgr._sync_with_master,
-                                  mq_channel, 'master'))
+                                  args=(packets, rmq_conn_params, 'master'))
         master_thread.start()
-        time.sleep(0.1)
+
+        time.sleep(1)
         try:
             while packets:
                 packet = packets.pop(0)
@@ -68,7 +68,7 @@ class TestTask(TestCase):
                 self.assertEqual(msg['object'], packet[1].to_dict())
                 self.assertEqual(msg['type'], packet[0])
         except Exception as ex:
-            print(body)
+            print(ex)
             print(json.loads(body))
             master_thread.join()
             mq_channel.queue_delete(queue='master')
