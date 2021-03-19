649ecd17172923c8225538bbf1cb57c619418477
Ioannis Paraskevakos <g.paraskev@rutgers.edu>
2021-03-19 15:54:29 -0400

    Sync thread failure integration test

---
+++ tests/test_integration/test_amgr/test_amgr.py
@@ -0,0 +1,77 @@
+# pylint: disable=protected-access, unused-argument
+# pylint: disable=no-value-for-parameter
+
+from unittest   import TestCase
+from hypothesis import given, settings
+import threading as mt
+import timeout_decorator
+
+
+import hypothesis.strategies as st
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+import radical.utils as ru
+import radical.entk.exceptions as ree
+
+from radical.entk import AppManager as Amgr
+import radical.entk as re
+
+
+class TestBase(TestCase):
+
+    # --------------------------------------------------------------------------
+    #
+    @mock.patch.object(Amgr, '__init__', return_value=None)
+    @mock.patch.object(Amgr, '_submit_rts_tmgr', return_value=True)
+    @mock.patch('radical.entk.execman.mock.ResourceManager',
+                return_value=mock.MagicMock(get_resource_allocation_state=mock.MagicMock(return_value='RUNNING'),
+                                            get_completed_states=mock.MagicMock(return_value=['DONE']),
+                                            get_rts_info=mock.MagicMock(return_value={'pilot': 'pilot.0000'})
+                                            ))
+    @mock.patch('radical.entk.appman.wfprocessor.WFprocessor',
+                return_value=mock.MagicMock(workflow_incomplete=mock.MagicMock(return_value=True),
+                                            check_processor=mock.MagicMock(return_value=True),
+                                            terminate_processor=mock.MagicMock(return_value=True),
+                                            start_processor=mock.MagicMock(return_value=True)
+                                            ))
+    @mock.patch('radical.entk.execman.mock.TaskManager',
+                return_value=mock.MagicMock(check_heartbeat=mock.MagicMock(return_value=True),
+                                            terminate_heartbeat=mock.MagicMock(return_value=True),
+                                            terminate_manager=mock.MagicMock(return_value=True),
+                                            start_manager=mock.MagicMock(return_value=True),
+                                            start_heartbeat=mock.MagicMock(return_value=True)
+                                            ))
+    @mock.patch('radical.utils.Profiler')
+    @mock.patch('radical.utils.Logger')
+    def test_run_workflow(self, mocked_init, mocked_submit_rts_tmgr, mocked_ResourceManager,
+                          mocked_WFprocessor, mocked_TaskManager, mocked_Profiler,
+                          mocked_Logger):
+        os.environ['RU_RAISE_ON_SYNC_FAIL']='5'
+        hostname = os.environ.get('RMQ_HOSTNAME', 'localhost')
+        port = int(os.environ.get('RMQ_PORT', '5672'))
+        username = os.environ.get('RMQ_USERNAME')
+        password = os.environ.get('RMQ_PASSWORD')
+        appman = Amgr(hostname=hostname, port=port, username=username,
+            password=password)
+        appman._wfp = re.appman.wfprocessor.WFprocessor()
+        appman._task_manager = re.execman.mock.TaskManager()
+        appman._rmgr = re.execman.mock.ResourceManager()
+        appman._uid = 'appman.0000'
+        appman._logger = mocked_Logger
+        appman._prof = mocked_Profiler
+        pipe = mock.Mock()
+        pipe.lock = mt.Lock()
+        pipe.completed = False
+        pipe.uid = 'pipe.0000'
+        appman._workflow = set([pipe])
+        appman._cur_attempt = 1
+        appman._reattempts = 3
+
+        with self.assertRaises(ree.EnTKError):
+            appman._run_workflow()
+
+        self.assertEqual(appman._cur_attempt, 4)
