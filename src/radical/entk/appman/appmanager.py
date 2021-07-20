e3c4c0fd3761b9ede3869c5aff5c3965a5595b67
Hyungro Lee <hroe.lee@gmail.com>
2021-07-20 18:27:40 -0400

    explicit naming for new args

--- src/radical/entk/appman/appmanager.py
+++ src/radical/entk/appman/appmanager.py
@@ -58,6 +58,8 @@ class AppManager(object):
                           True/False} when RTS is RP
         :name:            Name of the Application. It should be unique between
                           executions. (default is randomly assigned)
+        :base_path:       base path of logger and profiler with a session id,
+                          None (default) will use a current working directory
     '''
 
     # --------------------------------------------------------------------------
@@ -76,7 +78,7 @@ class AppManager(object):
                  rmq_cleanup=None,
                  rts_config=None,
                  name=None,
-                 config=None):
+                 base_path=None):
 
         # Create a session for each EnTK script execution
         if name:
@@ -86,20 +88,17 @@ class AppManager(object):
             self._name = str()
             self._sid  = ru.generate_id('re.session', ru.ID_PRIVATE)
 
-        if config is None:
-            config = dict()
-
         self._read_config(config_path, hostname, port, username, password,
                           reattempts, resubmit_failed, autoterminate,
                           write_workflow, rts, rmq_cleanup, rts_config,
-                          config)
+                          base_path)
 
         # Create an uid + logger + profiles for AppManager, under the sid
         # namespace
 
         self._uid = ru.generate_id('appmanager.%(counter)04d', ru.ID_CUSTOM)
 
-        path = self._path + '/' + self._sid
+        path = str(self._base_path) + '/' + self._sid
         name = 'radical.entk.%s' % self._uid
 
         self._logger = ru.Logger(name=name, path=path)
@@ -143,21 +142,18 @@ class AppManager(object):
     #
     def _read_config(self, config_path, hostname, port, username, password,
                      reattempts, resubmit_failed, autoterminate,
-                     write_workflow, rts, rmq_cleanup, rts_config, config_params):
+                     write_workflow, rts, rmq_cleanup, rts_config, base_path):
 
         if not config_path:
             config_path = os.path.dirname(os.path.abspath(__file__))
 
         config = ru.read_json(os.path.join(config_path, 'config.json'))
 
-        # config_params overwrites values if duplicated
-        config.update(config_params)
-
         def _if(val1, val2):
             if val1 is not None: return val1
             else               : return val2
 
-        self._path             = str(config.get('base_path', os.getcwd()))
+        self._base_path        = _if(base_path,       os.getcwd())
         self._hostname         = _if(hostname,        config['hostname'])
         self._port             = _if(port,            config['port'])
         self._username         = _if(username,        config['username'])
