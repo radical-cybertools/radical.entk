5af9d8fd6842a8fdbbf6d97db3238c88d17d0de4
Mikhail Titov <mikhail.a.titov@gmail.com>
2022-07-28 18:46:02 -0400

    introduced new Task attributes `pre/post_launch` (mapped to RP TaskDescription attributes `pre/post_launch`); and updated tests

--- tests/test_integration/test_tmgr_rp/test_tproc_rp.py
+++ tests/test_integration/test_tmgr_rp/test_tproc_rp.py
@@ -1,23 +1,19 @@
-# pylint: disable=protected-access, unused-argument
-# pylint: disable=no-value-for-parameter
-
-from unittest import TestCase
-
-from radical.entk.execman.rp.task_processor import create_td_from_task
-from radical.entk                           import Task
-
-try:
-    import mock
-except ImportError:
-    from unittest import mock
+# pylint: disable=protected-access,unused-argument,no-value-for-parameter
 
 import pickle
 import radical.pilot as rp
 
+from unittest import mock, TestCase
 
+from radical.entk.execman.rp.task_processor import create_td_from_task
+from radical.entk                           import Task
+
+
+# ------------------------------------------------------------------------------
+#
 class TestBase(TestCase):
 
-    # ------------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     #
     def test_create_td_from_task(self):
 
@@ -30,50 +26,55 @@ class TestBase(TestCase):
             pipeline_name: {
                 stage_name: {
                     t1_name: {
-                        'path'   : '/home/vivek/t1',
-                        'uid': 'task.0000'
+                        'path': '/home/vivek/t1',
+                        'uid' : 'task.0000'
                     },
                     t2_name: {
-                        'path'   : '/home/vivek/t2',
-                        'uid': 'task.0003'
+                        'path': '/home/vivek/t2',
+                        'uid' : 'task.0003'
                     }
                 }
             }
         }
 
         task = Task({'uid': 'task.0000'})
-        task.name = 'task.0000'
-        task.parent_stage = {'uid' : 'stage.0000',
-                             'name' : 'stage.0000'}
+        task.name            = 'task.0000'
+        task.parent_stage    = {'uid' : 'stage.0000',
+                                'name': 'stage.0000'}
         task.parent_pipeline = {'uid' : 'pipe.0000',
-                                'name' : 'pipe.0000'}
-        task.pre_exec = ['post_exec']
-        task.executable = '/bin/date'
-        task.arguments = ['test_args']
-        task.sandbox = 'unit.0000'
-        task.post_exec = ['']
-        task.cpu_reqs = {'cpu_processes': 5,
-                         'cpu_threads': 6,
-                         'cpu_process_type': 'MPI',
-                         'cpu_thread_type': None}
-        task.gpu_reqs = {'gpu_processes': 5,
-                         'gpu_threads': 6,
-                         'gpu_process_type': None,
-                         'gpu_thread_type': None}
+                                'name': 'pipe.0000'}
+        task.pre_launch      = ['pre_launch_cmd']
+        task.pre_exec        = ['pre_exec_cmd']
+        task.executable      = '/bin/date'
+        task.arguments       = ['test_args']
+        task.sandbox         = 'task.0000'
+        task.post_exec       = ['']
+        task.post_launch     = []
+        task.cpu_reqs        = {'cpu_processes'   : 5,
+                                'cpu_threads'     : 6,
+                                'cpu_process_type': 'MPI',
+                                'cpu_thread_type' : None}
+        task.gpu_reqs        = {'gpu_processes'   : 5,
+                                'gpu_threads'     : 6,
+                                'gpu_process_type': None,
+                                'gpu_thread_type' : None}
 
         task.lfs_per_process = 235
-        task.stderr = 'stderr'
-        task.stdout = 'stdout'
+        task.stderr          = 'stderr'
+        task.stdout          = 'stdout'
+
         hash_table = {}
         test_td = create_td_from_task(task, placeholders, hash_table,
                                       '.test.pkl', 'test_sid', mock.Mock())
         self.assertIsInstance(test_td, rp.TaskDescription)
         self.assertEqual(test_td.name, 'task.0000,task.0000,stage.0000,stage.0000,pipe.0000,pipe.0000')
-        self.assertEqual(test_td.pre_exec, ['post_exec'])
+        self.assertEqual(test_td.pre_launch, ['pre_launch_cmd'])
+        self.assertEqual(test_td.pre_exec, ['pre_exec_cmd'])
         self.assertEqual(test_td.executable, '/bin/date')
         self.assertEqual(test_td.arguments, ['test_args'])
-        self.assertEqual(test_td.sandbox, 'unit.0000')
+        self.assertEqual(test_td.sandbox, 'task.0000')
         self.assertEqual(test_td.post_exec, [''])
+        self.assertEqual(test_td.post_launch, [])
         self.assertEqual(test_td.cpu_processes, 5)
         self.assertEqual(test_td.cpu_threads, 6)
         self.assertEqual(test_td.cpu_process_type, rp.MPI)
@@ -87,15 +88,18 @@ class TestBase(TestCase):
         self.assertEqual(test_td.stderr, 'stderr')
         self.assertEqual(test_td.input_staging, [])
         self.assertEqual(test_td.output_staging, [])
-        self.assertEqual(test_td.tags, {'colocate':'task.0000'})
-        self.assertEqual(test_td.uid,'task.0000')
+        self.assertEqual(test_td.tags, {'colocate': 'task.0000'})
+        self.assertEqual(test_td.uid, 'task.0000')
         self.assertEqual(hash_table, {'task.0000': 'task.0000'})
         task.tags = {'colocate': 'task.0001'}
         test_td = create_td_from_task(task, placeholders, hash_table,
                                       '.test.pkl', 'test_sid', mock.Mock())
-        self.assertEqual(test_td.tags, {'colocate':'task.0003'})
+        self.assertEqual(test_td.tags, {'colocate': 'task.0003'})
         self.assertEqual(test_td.uid, 'task.0000.0000')
         self.assertEqual(hash_table, {'task.0000': 'task.0000.0000'})
         with open('.test.pkl', 'rb') as f:
             submitted_tasks = pickle.load(f)
         self.assertEqual(submitted_tasks, hash_table)
+
+# ------------------------------------------------------------------------------
+
