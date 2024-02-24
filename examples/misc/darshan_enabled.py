a3476af6ed5c87dc079847b3c57caf8f04906c19
Mikhail Titov <mikhail.a.titov@gmail.com>
2024-02-24 00:18:06 -0500

    updated Darshan example according to the latest changes

--- examples/misc/darshan_enabled.py
+++ examples/misc/darshan_enabled.py
@@ -5,7 +5,10 @@ import os
 import radical.entk  as re
 import radical.pilot as rp
 
-from radical.entk.utils.provenance import enable_darshan, get_provenance_graph
+from radical.entk.utils.provenance import (cache_darshan_env,
+                                           darshan,
+                                           enable_darshan,
+                                           get_provenance_graph)
 
 RESOURCE_DESCRIPTION = {
     # https://radicalpilot.readthedocs.io/en/stable/supported/polaris.html
@@ -19,9 +22,11 @@ RESOURCE_DESCRIPTION = {
 os.environ['RADICAL_LOG_LVL'] = 'DEBUG'
 os.environ['RADICAL_REPORT']  = 'TRUE'
 
+TASK_01_OUTPUT = 'output_01.dat'
+
 
 # pylint: disable=anomalous-backslash-in-string
-def get_stages():
+def get_stage_0():
 
     # hello-RP task
     task_00 = re.Task({
@@ -33,41 +38,47 @@ def get_stages():
     })
 
     # R/W data
-    output_01 = 'output_01.dat'
     task_01   = re.Task({
         'executable'       : '/bin/sh',
-        'arguments'        : ['-c', f'cat input.dat | wc > {output_01}'],
+        'arguments'        : ['-c', f'cat input.dat | wc > {TASK_01_OUTPUT}'],
         'upload_input_data': ['/etc/passwd > input.dat'],
-        'copy_output_data' : [f'{output_01} > $SHARED/{output_01}']
+        'copy_output_data' : [f'{TASK_01_OUTPUT} > $SHARED/{TASK_01_OUTPUT}']
     })
 
     stage_0 = re.Stage()
-    stage_0.add_tasks([task_00, task_01])
+    # --- enable Darshan for task "task_01" only
+    stage_0.add_tasks([task_00, enable_darshan(task_01)])
+    return stage_0
+
+
+# --- enable Darshan for the whole "stage_1" using decorator
+@darshan
+def get_stage_1():
 
     # R/W data and task depends on the task from the previous stage
     task_10 = re.Task({
         'executable'     : '/bin/sh',
         'arguments'      : ['-c',
-                            f"sed -r 's/\s+//g' {output_01} " +     # noqa: W605
+                            f"sed -r 's/\s+//g' {TASK_01_OUTPUT} " +  # noqa: W605
                             '| grep -o . | sort | uniq -c > output_10.dat'],
-        'copy_input_data': [f'$SHARED/{output_01} > {output_01}']
+        'copy_input_data': [f'$SHARED/{TASK_01_OUTPUT} > {TASK_01_OUTPUT}']
     })
 
     stage_1 = re.Stage()
     stage_1.add_tasks([task_10])
-
-    return [stage_0, stage_1]
+    return stage_1
 
 
 def main():
-    pipeline = re.Pipeline()
-    pipeline.add_stages(get_stages())
-    workflow = [pipeline]
 
-    enable_darshan(pipelines=workflow,
-                   modules=['e4s/22.08/PrgEnv-gnu',
-                            'darshan-runtime',
-                            'darshan-util'])
+    cache_darshan_env(darshan_runtime_root='$DARSHAN_RUNTIME_ROOT',
+                      modules=['e4s/22.08/PrgEnv-gnu',
+                               'darshan-runtime',
+                               'darshan-util'])
+
+    pipeline = re.Pipeline()
+    pipeline.add_stages([get_stage_0(), get_stage_1()])
+    workflow = [pipeline]
 
     amgr = re.AppManager()
     amgr.resource_desc = RESOURCE_DESCRIPTION
